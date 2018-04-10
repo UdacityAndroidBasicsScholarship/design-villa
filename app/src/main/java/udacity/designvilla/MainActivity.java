c889ba93b19e67e02bdbb809403e57f0ede7ef25
Akshit Jain <akshitjain@Amans-MacBook-Air.local>
2018-04-10 20:53:00 +0530

    Initial Commit

---
+++ app/src/main/java/udacity/designvilla/MainActivity.java
@@ -0,0 +1,29 @@
+package udacity.designvilla;
+
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Handler;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int timeOut = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, timeOut);
+    }
+}
