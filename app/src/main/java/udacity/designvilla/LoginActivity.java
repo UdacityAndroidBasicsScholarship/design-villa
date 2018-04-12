9514d1e704c0ca58a11271808bf48bb0c317ad50
Lianding <liandingkhuptong178@gmail.com>
2018-04-12 23:34:42 +0530

    Changes: Activity names, hardcoded values and variable names.

--- app/src/main/java/udacity/designvilla/LoginActivity.java
+++ app/src/main/java/udacity/designvilla/LoginActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import com.example.android.splashscreenjava.R;
 
 
-public class SecondActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_login);
     }
 }
