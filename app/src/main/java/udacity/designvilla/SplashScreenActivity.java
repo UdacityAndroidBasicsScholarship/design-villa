e8eb4acdc922aad2ef9ef7d635aa90ca270dafe7
Vaishnavi Janardhan <35659656+vaishnavi-janardhan@users.noreply.github.com>
2018-04-16 22:16:37 +0530

    Added new logo and splash screen animations

--- app/src/main/java/udacity/designvilla/SplashScreenActivity.java
+++ app/src/main/java/udacity/designvilla/SplashScreenActivity.java
@@ -3,12 +3,19 @@ package udacity.designvilla;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.android.splashscreenjava.R;
+import com.podcopic.animationlib.library.AnimationType;
+import com.podcopic.animationlib.library.StartSmartAnimation;
+
+import java.util.Objects;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -17,12 +24,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_splashscreen);
 
-        ImageView logo = findViewById(R.id.imageView);
-        Animation bounce = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce_animation);
-        logo.startAnimation(bounce);
+        ImageView splashLogo = findViewById(R.id.splash_logo);
+        LinearLayout splashText = findViewById(R.id.splash_text);
+        StartSmartAnimation.startAnimation( splashLogo , AnimationType.Wave , TIME_OUT , 0 , false );
+        StartSmartAnimation.startAnimation( splashText , AnimationType.ZoomIn , TIME_OUT , 0 , false );
 
         new android.os.Handler().postDelayed(new Runnable() {
             @Override
@@ -30,7 +38,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                 Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
-                overridePendingTransition(R.anim.sliding_in, R.anim.sliding_out);
             }
         }, TIME_OUT);
     }
