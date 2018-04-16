1f1b73e5d47643665176378cd31ae742ddd7e6fd
sabdar18 <developer.sabdar@gmail.com>
2018-04-16 17:17:45 +0530

    add Google Signin Button and change Colour theme, For Google Login Button add dependencies to gradle file

--- app/src/main/java/udacity/designvilla/LoginActivity.java
+++ app/src/main/java/udacity/designvilla/LoginActivity.java
@@ -2,16 +2,33 @@ package udacity.designvilla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.android.splashscreenjava.R;
+import com.google.android.gms.common.SignInButton;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SignInButton mGoogleSignInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        mGoogleSignInButton = findViewById(R.id.google_sign_in_button);
+        mGoogleSignInButton.setSize(SignInButton.SIZE_WIDE);
+        mGoogleSignInButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.google_sign_in_button) {
+            Toast.makeText(this, R.string.login_message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
