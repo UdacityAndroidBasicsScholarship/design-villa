ac333419ac97a9834efd3bfbe7ae7f649a47cbe9
DSdatsme <darsuratwala@gmail.com>
2018-04-16 22:22:43 +0530

    Splash & Login

--- app/src/main/java/udacity/designvilla/LoginActivity.java
+++ app/src/main/java/udacity/designvilla/LoginActivity.java
@@ -1,16 +1,65 @@
 package udacity.designvilla;
 
+import android.animation.ObjectAnimator;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.android.splashscreenjava.R;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button button;
+    private TextView text;
+    private ViewGroup transitionsContainer;
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+//this lines are for gadient background
+        relativeLayout = (RelativeLayout)findViewById(R.id.login_screen_relative_layout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
+
+
+
+
+        transitionsContainer = (ViewGroup)findViewById(R.id.transitions_container);
+        text = (TextView) transitionsContainer.findViewById(R.id.logging_in_textview);
+        button = (Button) transitionsContainer.findViewById(R.id.login_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            boolean visible;
+
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(transitionsContainer);
+                visible = !visible;
+                text.setVisibility(visible ? View.VISIBLE : View.GONE);
+            }
+
+        });
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
+    }
+
 }
--- app/src/main/java/udacity/designvilla/SplashScreenActivity.java
+++ app/src/main/java/udacity/designvilla/SplashScreenActivity.java
@@ -1,17 +1,24 @@
 package udacity.designvilla;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.splashscreenjava.R;
 
-public class SplashScreenActivity extends AppCompatActivity {
+import static java.lang.System.in;
 
+public class SplashScreenActivity extends AppCompatActivity {
+    final Animation in = new AlphaAnimation(0.0f, 1.0f);
     public final static int TIME_OUT = 3000;
 
     @Override
@@ -19,8 +26,11 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splashscreen);
+        startAnimation();
 
         ImageView logo = findViewById(R.id.imageView);
+        TextView tagline = (TextView)findViewById(R.id.tagline_textView);
+
         Animation bounce = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce_animation);
         logo.startAnimation(bounce);
 
@@ -33,5 +43,19 @@ public class SplashScreenActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.sliding_in, R.anim.sliding_out);
             }
         }, TIME_OUT);
+
+        //Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.sliding_in);
+        //tagline.startAnimation(fadeIn);
+        in.setDuration(3000);
+        tagline.startAnimation(in);
+
+
+    }
+    private void startAnimation(){
+        ProgressBar mProgressBar = (ProgressBar) findViewById(R.id.pb_loading);
+       ObjectAnimator progressAnimator = ObjectAnimator.ofInt(mProgressBar, "progress", 0, 100);
+        progressAnimator.setDuration(3000);
+        progressAnimator.setInterpolator(new LinearInterpolator());
+        progressAnimator.start();
     }
 }
