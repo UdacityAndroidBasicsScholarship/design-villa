af5866c7673fffdf039287a3ea69f9a13494cfbe
vipul Aggarwal <vipul.captureart@gmail.com>
2018-04-16 22:29:34 +0530

    Update MainActivity.java

--- app/src/main/java/udacity/designvilla/MainActivity.java
+++ app/src/main/java/udacity/designvilla/MainActivity.java
@@ -1,31 +1,36 @@
 package udacity.designvilla;
 
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.content.Intent;
-        import android.os.Handler;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-        import com.example.android.splashscreenjava.R;
+import com.example.android.splashscreenjava.R;
 
-public class MainActivity extends AppCompatActivity {
 
-    public static int timeOut = 3000;
+public class LoginActivity extends AppCompatActivity {
+
+    RelativeLayout rellay, rellay1;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay.setVisibility(View.VISIBLE);
+            rellay1.setVisibility(View.VISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        new android.os.Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, Splashscreen.class);
-                startActivity(intent);
-                finish();
-            }
-        }, timeOut);
+        rellay = (RelativeLayout) findViewById(R.id.rellay);
+        rellay = (RelativeLayout) findViewById(R.id.rellay1);
+
+        handler.postDelayed(runnable, 3000); //3000 is the timeout for the splash
     }
 }
