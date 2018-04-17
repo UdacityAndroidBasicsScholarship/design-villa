416e8901b68c99a3948a7cb528d8d39b0bac418c
Your Name <you@example.com>
2018-04-17 11:42:03 +0530

    Edit splash screen xml and java file and login xml only
    Added some shapes , icons , logo , styles , string resources and change app theme.

--- app/src/main/java/udacity/designvilla/SplashScreenActivity.java
+++ app/src/main/java/udacity/designvilla/SplashScreenActivity.java
@@ -1,10 +1,8 @@
 package udacity.designvilla;
 
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.content.Intent;
-
-        import com.example.android.splashscreenjava.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -13,16 +11,11 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_splashscreen);
 
-        new android.os.Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, TIME_OUT);
+        startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+
+        // close splash activity
+        finish();
+
     }
 }
