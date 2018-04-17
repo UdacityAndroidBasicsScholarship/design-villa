ef7ee1bf9900e81e175ff208f795c50c118f9699
Rohit Vivek <rohitvivek1234@gmail.com>
2018-04-17 19:52:17 +0530

    Splash & Login Screen
    Splash & Login Screen

--- app/src/main/java/udacity/designvilla/LoginActivity.java
+++ app/src/main/java/udacity/designvilla/LoginActivity.java
@@ -1,17 +1,17 @@
 package udacity.designvilla;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.splashscreenjava.R;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
     }
+
 }
