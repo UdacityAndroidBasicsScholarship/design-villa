7a420ba9b27f80c71eb43fcdbf5e5f37cd5d62d9
la2798 <38189248+la2798@users.noreply.github.com>
2018-04-18 12:43:57 +0530

    New changes - changed splash screen and login screen

--- app/src/main/java/udacity/designvilla/LoginActivity.java
+++ app/src/main/java/udacity/designvilla/LoginActivity.java
@@ -1,16 +1,60 @@
 package udacity.designvilla;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.android.splashscreenjava.R;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText name;
+    private EditText password;
+    // private TextView info;
+    private Button login;
+    private int noOfAttempts = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+            getSupportActionBar().hide();
+
         setContentView(R.layout.activity_login);
+
+        name = (EditText)findViewById(R.id.loginName);
+        password = (EditText)findViewById(R.id.password);
+        //   info = (TextView)findViewById(R.id.info);
+        login = (Button)findViewById(R.id.Loginbutton);
+
+        //     info.setText("No. of attempts remaining : 3");
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+
+    private void validate (String userName , String userPassword){
+        if((userName.equals("admin") ) && (userPassword.equals("1234"))) {
+            Intent intent = new Intent(LoginActivity.this, afterLogin.class);
+            startActivity(intent);
+        }
+        else{
+            noOfAttempts--;
+
+            //       info.setText("No. of attempts remaining : " + String.valueOf(noOfAttempts));
+
+            if(noOfAttempts == 0){
+                login.setEnabled(false);
+
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/udacity/designvilla/afterLogin.java
@@ -0,0 +1,19 @@
+package udacity.designvilla;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.splashscreenjava.R;
+
+/**
+ * Created by Lakshmi on 18-Apr-18.
+ */
+
+public class afterLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_login);
+    }
+}
