6c04f7317c96620bdb61439dfdd8bbdc4392f331
rohitvivek <rohitvivek1234@gmail.com>
2018-04-18 19:40:47 +0530

    Splash with animation

--- app/src/main/java/udacity/designvilla/SplashScreenActivity.java
+++ app/src/main/java/udacity/designvilla/SplashScreenActivity.java
@@ -3,11 +3,18 @@ package udacity.designvilla;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.android.splashscreenjava.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    ImageView imageView;
 
+    // Animation
+    Animation animation;
     public final static int TIME_OUT = 3000;
 
     @Override
@@ -24,5 +31,9 @@ public class SplashScreenActivity extends AppCompatActivity {
                 finish();
             }
         }, TIME_OUT);
+       imageView = (ImageView) findViewById(R.id.image_view);
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fadein);
+        imageView.startAnimation(animation);
     }
 }
