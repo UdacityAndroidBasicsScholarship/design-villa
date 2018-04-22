46dc1da8e1d083977c24929a90ff16b58c152c8b
Gopal Dan <gopal125dan@gmail.com>
2018-04-22 13:22:57 +0530

    Added Viewpager with login activity

--- app/src/main/java/udacity/designvilla/LoginActivity.java
+++ app/src/main/java/udacity/designvilla/LoginActivity.java
@@ -1,16 +1,15 @@
 package udacity.designvilla;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.android.splashscreenjava.R;
-
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        //getSupportActionBar().hide();
+        setContentView(R.layout.log_in_activity);
     }
 }
+
+
