47e9f8fa7e355c5087ec0eb97c4d5652fa5c4eac
Rohit Bhargava <rohitbhargava586@gmail.com>
2018-04-26 00:02:06 +0530

    1. Added Splash Screen and Login Activity 2. Animations applied on Splash screen. 3. Added Google SignIn button. 4. Added fonts flavors. 5. Updated google-services in build.gradle (app level) 5. Removed unused imports. 6. Deleted unnecessary files. 7. Cannot go back to Splash Screen Activity.

--- app/src/main/java/udacity/designvilla/SplashScreenActivity.java
+++ app/src/main/java/udacity/designvilla/SplashScreenActivity.java
@@ -1,37 +1,70 @@
 package udacity.designvilla;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import com.example.android.splashscreenjava.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     public final static int TIME_OUT = 3000;
+    private String versionName = BuildConfig.VERSION_NAME;
+
+    RelativeLayout relativeLayout_parent, relativeLayout_parent2;
+    ImageView imageView_logo;
+    Button enter_btn;
+    TextView textView_tag;
+    TextView textView_dev;
+    TextView textView_ver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_splashscreen);
 
-        ImageView logo = findViewById(R.id.imageView);
-        Animation bounce = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce_animation);
-        logo.startAnimation(bounce);
+        relativeLayout_parent = findViewById(R.id.parent);
+        relativeLayout_parent2 = findViewById(R.id.image_bg);
+        imageView_logo = findViewById(R.id.img_villa);
+        enter_btn = findViewById(R.id.enter_btn);
+        textView_tag = findViewById(R.id.tag_textview);
+        textView_dev = findViewById(R.id.dev_textView);
+        textView_ver = findViewById(R.id.textView_ver);
 
-        new android.os.Handler().postDelayed(new Runnable() {
+        textView_ver.setText(versionName);
+
+        Animation fade = AnimationUtils.loadAnimation(SplashScreenActivity.this, R.anim.fade_animate);
+        imageView_logo.startAnimation(fade);
+        textView_tag.startAnimation(fade);
+        textView_dev.startAnimation(fade);
+        textView_ver.startAnimation(fade);
+
+        Animation slide = AnimationUtils.loadAnimation(SplashScreenActivity.this, R.anim.sliding_in);
+        enter_btn.startAnimation(slide);
+
+        enter_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
-                overridePendingTransition(R.anim.sliding_in, R.anim.sliding_out);
             }
-        }, TIME_OUT);
+        });
+
     }
 }
