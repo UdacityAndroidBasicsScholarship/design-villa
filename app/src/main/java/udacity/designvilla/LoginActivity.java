d2101af48fe759caa417ce8a5ecbcca83358733e
Aditya <yaditya20@gmail.com>
2018-04-25 15:25:43 +0530

    New Changes

--- app/src/main/java/udacity/designvilla/LoginActivity.java
+++ app/src/main/java/udacity/designvilla/LoginActivity.java
@@ -1,17 +1,41 @@
 package udacity.designvilla;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import com.example.android.splashscreenjava.R;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    RelativeLayout relativeLayout;
+    AnimationDrawable animationDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        relativeLayout = findViewById(R.id.relativeLayout1);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(1000);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning())
+            animationDrawable.stop();
     }
 }
--- app/src/main/java/udacity/designvilla/SplashScreenActivity.java
+++ app/src/main/java/udacity/designvilla/SplashScreenActivity.java
@@ -1,24 +1,33 @@
 package udacity.designvilla;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.android.splashscreenjava.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    public final static int TIME_OUT = 3000;
+    public final static int TIME_OUT = 7000;
+    RelativeLayout relativeLayout;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splashscreen);
+
+        relativeLayout = findViewById(R.id.relativeLayout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(1000);
 
         ImageView logo = findViewById(R.id.imageView);
         Animation bounce = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce_animation);
@@ -33,5 +42,19 @@ public class SplashScreenActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.sliding_in, R.anim.sliding_out);
             }
         }, TIME_OUT);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning())
+            animationDrawable.stop();
     }
 }
