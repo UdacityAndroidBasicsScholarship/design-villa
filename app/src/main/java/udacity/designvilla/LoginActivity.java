ac5f215bdef5aaffd285aa1528067380e5bda296
Rohit Bhargava <rohitbhargava586@gmail.com>
2018-04-27 12:25:35 +0530

    **Splash branch Activity** 1. Added new background to Splash and Login 2. Animation for Splash screen activity 3. Added Google Sign in button for Login 4. Added Google SignIn and Firebase support libraries 5. A status shown in login whether the user has already logged in or not 6. A loading dialog box integrated while signing in.

--- app/src/main/java/udacity/designvilla/LoginActivity.java
+++ app/src/main/java/udacity/designvilla/LoginActivity.java
@@ -1,31 +1,177 @@
 package udacity.designvilla;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.android.splashscreenjava.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-import udacity.designvilla.Util.Tools;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    ImageView imageView;
+    TextView text_dev;
 
-public class LoginActivity extends AppCompatActivity {
+    TextView mStatusTextView;
 
-    private SignInButton googleSignInBtn;
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton mGoogleSignInButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_login);
-        //customize the status bar color and light
-        Tools.setSystemBarColor(this, android.R.color.white);
-        Tools.setSystemBarLight(this);
 
-        googleSignInBtn = findViewById(R.id.sign_in_button);
-        //customize the button
-        googleSignInBtn.setSize(SignInButton.SIZE_WIDE);
-        googleSignInBtn.setColorScheme(SignInButton.COLOR_DARK);
+        imageView = findViewById(R.id.img_villa);
+        text_dev = findViewById(R.id.dev_textView);
 
-        //TODO: Implement Google Sign-In logic
+        mStatusTextView = findViewById(R.id.status);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+        mGoogleSignInButton = findViewById(R.id.sign_in_button);
+        mGoogleSignInButton.setOnClickListener(this);
+        mGoogleSignInButton.setSize(SignInButton.SIZE_WIDE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account);
+            Log.i(TAG, account.getDisplayName());
+        }
+        catch (ApiException e) {
+            Log.w(TAG, "TAG:failed code=" + e.getStatusCode());
+            Toast.makeText(this, ":" + e.getStatusCode(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        showProgressDialog();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                }
+                else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.getException());
+                    Snackbar.make(findViewById(R.id.login_bg), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+                hideProgressDialog();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+        hideProgressDialog();
+
+        if (user != null) {
+            mStatusTextView.setText(getString(R.string.signed_in_user, user.getEmail()));
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+        }
+        else {
+            mStatusTextView.setText(R.string.signed_out);
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 }
