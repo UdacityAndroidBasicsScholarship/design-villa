56fa3422bbe87362a4d0c32bd4eb35f77247c367
srdpatel <sagarpatel288@gmail.com>
2018-05-04 07:34:30 +0530

    Add utils (All package name should be in lower case)

---
+++ app/src/main/java/udacity/designvilla/listeners/OnSelectImageListener.java
@@ -0,0 +1,15 @@
+package udacity.designvilla.listeners;
+
+/**
+ * Created by srdpatel on 2/25/2018.
+ * <p>
+ * Use to get image path of selected image from the gallery.
+ * Use {@link android.support.v4.content.FileProvider} or {@link udacity.designvilla.Util.FileUtils} to get image path for image taken from camera.
+ * <p>
+ *
+ * @since 1.0
+ */
+
+public interface OnSelectImageListener {
+    void onSelectImage(String imagePath);
+}
